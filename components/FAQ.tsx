1c3ec95fbfef895979b8f95b6889567d757548ca
Saira Shaikh <shyksaira27@gmail.com>
2024-08-03 07:17:19 -0700

    Your commit message

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,44 +1,88 @@
-import React from 'react'
+import React from "react";
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const FAQ = () => {
-    return (
-        <div className='space-y-16'>
-            <h1 className='text-5xl text-[#4A1F64] font-normal text-center'>Frequently Asked Questions</h1>
-
-            <Accordion type="single" collapsible className='text-[#4A1F64] text-xl'>
-                <AccordionItem value="item-1">
-                    <AccordionTrigger className='font-semibold'>Is it accessible?</AccordionTrigger>
-                    <AccordionContent className='bg-[#86629c]'>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                    <AccordionTrigger className='font-semibold'>Is it accessible?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                    <AccordionTrigger className='font-semibold'>Is it accessible?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-4">
-                    <AccordionTrigger className='font-semibold'>Is it accessible?</AccordionTrigger>
-                    <AccordionContent>
-                        Yes. It adheres to the WAI-ARIA design pattern.
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
+  return (
+    <div className="space-y-16">
+      <h1 className="text-5xl text-[#4A1F64] font-normal text-center">
+        Frequently Asked Questions
+      </h1>
+      <div className="flex gap-10 justify-between items-start py-2">
+        <Accordion
+          type="single"
+          collapsible
+          className="text-[#4A1F64] w-[80%] text-xl"
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger className="font-semibold">
+              Is it accessible?
+            </AccordionTrigger>
+            <AccordionContent className="bg-[#86629c]">
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-2">
+            <AccordionTrigger className="font-semibold">
+              Is it accessible?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-3">
+            <AccordionTrigger className="font-semibold">
+              Is it accessible?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-4">
+            <AccordionTrigger className="font-semibold">
+              Is it accessible?
+            </AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <div className="flex items-center flex-col gap-6 ">
+          <img src="faq boy.png" alt="" />
+          <div className="text-center">
+            {" "}
+            <h3 className="text-[#4A1F64] text-[30px] font-medium">
+              Any Question?
+            </h3>
+            <p className="text-[16px] text-[#AFAEAE]">
+              You can ask anything you want to know.
+            </p>
+          </div>
+          <div className="w-full">
+            {" "}
+            <h4 className="text-[18px] mb-2 text-gray-800 font-medium">
+              Let Us Know.
+            </h4>
+            <input
+              type="text"
+              placeholder="Enter Here.."
+              className="px-4 w-full py-2 rounded-3xl border border-gray-400 bg-transparent"
+            />
+          </div>
+          <button
+            type="submit"
+            className=" text-white w-fit px-[20px] rounded-[40px] shadow-md shadow-[#4A1F64] text-[18px] py-2 bg-gradient-to-br from-[#4A1F64] to-[#b792e7]"
+          >
+            Send
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default FAQ
+export default FAQ;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,46 @@
-'use client'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import ThemeSwitcher from '../utils/theme-switcher'
-import Navbar from './Navbar'
-import Image from 'next/image'
-
-
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import ThemeSwitcher from "../utils/theme-switcher";
+import Navbar from "./Navbar";
+import Image from "next/image";
 
 const Header = () => {
-    
-    return (
-        <div className='space-y-8 min-h-full h-auto'>
-           
-            <Navbar />
-            <div className='grid grid-cols-12'>
-                <div className=' col-span-8 z-10'>
-                    <div className='flex flex-col justify-center gap-2 h-full'>
-                        <h1 className='text-6xl font-semibold font-[josefin] text-[#4A1F64]'>Revolutionarize Education with <span className='text-[#A176BB]'>Personalized</span> Learning Paths</h1>
-                        <p className='text-lg text-[#5B2C78]'>Study any topic, anytime. Choose from a number of expert-led courses now.</p>
-                        <div className='my-8'>
-                            <Link href={'/courses'} className='text-[#ebe8ec] bg-gradient-to-l from-[#4A1F64] to-[#b792e7] py-2 px-6 rounded-full font-normal mt-8 border border-purple-800 shadow-md shadow-black'>Start Learning</Link>
-
-                        </div>
-                    </div>
-                </div>
-                <Image src={"/computer.png"} alt="hero" width={500} height={600} className="col-span-4 z-10 h-full" />
+  return (
+    <div className="space-y-8 min-h-full h-auto">
+      <Navbar />
+      <div className="grid grid-cols-12">
+        <div className=" col-span-8 z-10">
+          <div className="flex flex-col justify-center gap-2 h-full">
+            <h1 className="text-6xl font-semibold font-[josefin] text-[#4A1F64]">
+              Revolutionarize Education with{" "}
+              <span className="text-[#A176BB]">Personalized</span> Learning
+              Paths
+            </h1>
+            <p className="text-lg text-[#5B2C78]">
+              Study any topic, anytime. Choose from a number of expert-led
+              courses now.
+            </p>
+            <div className="my-8">
+              <Link
+                href={"/courses"}
+                className="text-[#ebe8ec] bg-gradient-to-l from-[#4A1F64] to-[#b792e7] py-2 px-6 rounded-full font-normal mt-8 border border-purple-800 shadow-md shadow-black"
+              >
+                Start Learning
+              </Link>
             </div>
+          </div>
         </div>
-    );
-}
+        <Image
+          src={"/computer.png"}
+          alt="hero"
+          width={500}
+          height={600}
+          className="col-span-4 z-10 h-full"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
